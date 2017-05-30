5d8d1a38bff9a5d158120c84fb538a7db357623d
Joe Berria <joeberria@gmail.com>
2017-05-30 17:56:25 -0600

    began working with escaping xml strings, not nuts about it right now

--- HackerNews/HackerStoryComment.swift
+++ HackerNews/HackerStoryComment.swift
@@ -45,10 +45,13 @@ class HackerStoryComments: NSObject, URLSessionDataDelegate {
     }
     
     private func fetchComment(byID id: Int) {
-        if let url = URL(string: baseURL + "\(id).json") {
-            let dataTask = session.dataTask(with: url)
-            dataTask.resume()
+        guard let url = URL(string: baseURL + "\(id).json") else {
+            print("Error fetchComment URL")
+            return
         }
+        
+        let dataTask = session.dataTask(with: url)
+        dataTask.resume()
     }
     
     //MARK: - URLSession delegate functions
@@ -62,22 +65,45 @@ class HackerStoryComments: NSObject, URLSessionDataDelegate {
     }
     
     func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        if let jsonString = String(bytes: data, encoding: .utf8) {
-            do {
-                if let respObj = try JSONSerialization.jsonObject(with: jsonString.data(using: .utf8)!) as? [String: Any] {
-                    let time = respObj["time"] as? Int ?? 0
-                    
-                    let cmt = comment(author: respObj["by"] as? String ?? "",
-                                      parentid: respObj["id"] as? Int ?? 0,
-                                      commentText: respObj["text"] as? String ?? "",
-                                      time: dateFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(time))))
-                    
-                    storyComments.append(cmt)
-                }
-                
-            } catch let err as NSError {
-                print("Story comment error \(err.debugDescription)")
-            }
+        guard let jsonString = String(bytes: data, encoding: .utf8) else {
+            print("Error comment urlSession jsonString")
+            return
         }
+        
+        do {
+            guard let respObj = try JSONSerialization.jsonObject(with: jsonString.data(using: .utf8)!) as? [String: Any] else {
+                print("Error comment urlSession JSONSerialization")
+                return
+            }
+            
+            let time = respObj["time"] as? Int ?? 0
+            let text = respObj["text"] as? String ?? ""
+            
+            let cmt = comment(author: respObj["by"] as? String ?? "",
+                              parentid: respObj["id"] as? Int ?? 0,
+                              commentText: text.escapeXML(),
+                              time: dateFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(time))))
+            
+            storyComments.append(cmt)
+            
+        } catch let err as NSError {
+            print("Story comment error \(err.debugDescription)")
+        }
+    }
+}
+
+extension String {
+    func escapeXML() -> String {
+        guard let newString = (self as NSString).removingPercentEncoding else {
+            return self
+        }
+        
+        //ugh, need to redesign this
+        return newString.replacingOccurrences(of: "&#x27;", with: "'")
+                        .replacingOccurrences(of: "&#x2F;", with: "/")
+                        .replacingOccurrences(of: "&quot;", with: "\"")
+                        .replacingOccurrences(of: "&gt;", with: ">")
+                        .replacingOccurrences(of: "&lt;", with: "<")
+                        .replacingOccurrences(of: "<p>", with: "\n")
     }
 }
