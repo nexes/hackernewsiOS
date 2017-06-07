337703133b18076d70b363d303e1c1cf5d8571df
Joe Berria <joeberria@gmail.com>
2017-06-06 22:14:48 -0600

    code cleanup, adding additional icons before 1.0 release.

--- HackerNews/AppDelegate.swift
+++ HackerNews/AppDelegate.swift
@@ -34,7 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return container
     }()
-
+    
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
